02fa2b4fcddd1e7ae5fa392c46bd7f2c665d4580
Joshua Lovins <joshualovins@joshuas-mbp.home>
2020-10-16 12:17:30 -0400

    Finished MVP and stretch for classes.js, prototypes.js, closure.js, and arrays-callbacks.js

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -43,5 +43,3 @@ const cuboid = new CuboidMaker(4, 5, 5);
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
-
-
